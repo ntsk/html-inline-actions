cffa08d681de591add833a30b14414acfa1015d5
ntsk <ntsk@ntsk.jp>
2025-09-28 21:41:07 +0900

    Fix Jest ESModule configuration

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
-module.exports = {
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coverageReporters: ['json-summary', 'text', 'lcov'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleFileExtensions: ['ts', 'js'],
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts'
-  ]
+  testMatch: ['**/*.test.ts'],
+  verbose: true
 };
