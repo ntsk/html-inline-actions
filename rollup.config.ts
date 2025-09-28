4cfc93f9850653de2dd5d0f0e8a0327789dbafb4
ntsk <ntsk@ntsk.jp>
2025-09-28 15:58:21 +0900

    Add Rollup bundling configuration

---
+++ rollup.config.ts
@@ -0,0 +1,19 @@
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: true
+  },
+  plugins: [
+    typescript(),
+    nodeResolve({
+      preferBuiltins: true
+    }),
+    commonjs()
+  ]
+}
