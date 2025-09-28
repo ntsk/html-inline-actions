a9f0d182d409071035bbd66d8fe613f334d1572c
ntsk <ntsk@ntsk.jp>
2025-09-28 15:42:59 +0900

    Disable TypeScript declaration files

--- dist/html-inline.d.ts
+++
@@ -1,8 +0,0 @@
-interface InlineOptions {
-    ignoreScripts?: boolean;
-    ignoreImages?: boolean;
-    ignoreLinks?: boolean;
-    ignoreStyles?: boolean;
-}
-export declare function inlineHtml(filePath: string, options?: InlineOptions): Promise<string>;
-export {};
